93f6fedb5ced828a814a40795818132989582da4
unknown <APRENDIZ@BOGDFPCGFSD774.FormacionSena.RED>
2024-06-28 12:38:02 -0500

    Segundo Despliegue de la api

--- config/database.js
+++ config/database.js
@@ -1,9 +1,16 @@
-const db={
+/* const db={
     host:'roundhouse.proxy.rlwy.net',
     port:50985,
     user:'root',
     password:'MtIbdTpJMmDsgVxQADfNotIYGOIPXVsc',
     database:'railway',
 }
-
+ */
+const db={
+    host:'localhost',
+    port:3306,
+    user:'root',
+    password:'',
+    database:'myapi',
+}
 export default db
