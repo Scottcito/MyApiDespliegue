10e942761de680ac525d252119fb90db342894d9
Scottcito <125676835+Scottcito@users.noreply.github.com>
2024-07-02 11:21:18 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,16 +1,18 @@
-/* const db={
-    host:'roundhouse.proxy.rlwy.net',
-    port:50985,
-    user:'root',
-    password:'MtIbdTpJMmDsgVxQADfNotIYGOIPXVsc',
-    database:'railway',
-}
- */
+/* 
 const db={
     host:'localhost',
     port:3306,
     user:'root',
     password:'',
     database:'myapi',
+}
+ */
+
+const db={
+    host:'roundhouse.proxy.rlwy.net',
+    port:50985,
+    user:'root',
+    password:'MtIbdTpJMmDsgVxQADfNotIYGOIPXVsc',
+    database:'railway',
 }
 export default db
